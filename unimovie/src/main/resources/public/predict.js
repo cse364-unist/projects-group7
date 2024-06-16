e202c3f9fb953255389b662d6d8239664f5c8ace
tastyminari <60382578+tastyminari@users.noreply.github.com>
2024-06-16 18:24:52 +0900

    Add files via upload

--- unimovie/src/main/resources/public/predict.js
+++ unimovie/src/main/resources/public/predict.js
@@ -1,34 +1,36 @@
 document.getElementById('predict-form').addEventListener('submit', async function(event) {
     event.preventDefault();
 
-    const formData = new FormData(this);
-    let postData = '';
+    const form = this;
+    const formData = new FormData(form);
+    let factor = '';
 
     formData.forEach((value, key) => {
-        postData += encodeURIComponent(key) + '=' + encodeURIComponent(value) + '&';
+        if (factor) {
+            factor += ',';
+        }
+        factor += value;
     });
 
-    postData = postData.slice(0, -1); // Remove the trailing '&'
     const apiUrl = 'http://localhost:8080/features/movie_predict';
+    const data = new FormData();
+    data.append('factor', factor);
 
     try {
         const response = await fetch(apiUrl, {
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: postData
+            body: data
         });
 
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
 
-        const responseData = await response.json(); 
+        const responseData = await response.json();
         console.log(responseData);
 
         if (responseData.result === 'HIT') {
-            window.location.href = '/HIT.html'; 
+            window.location.href = '/HIT.html';
         } else if (responseData.result === 'AVG') {
             window.location.href = '/AVG.html';
         } else {
